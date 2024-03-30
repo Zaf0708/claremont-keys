649b79e193646b1b09f3bac4a82462de110c6a5b
Carrimbaccus Zafroollah <zafroollah@gmail.com>
2024-03-30 13:14:13 +1100

    Remove carousel function.

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const slider = document.querySelector('.slider');
+const imgs = slider.children;
+let currImg = 0;
+
+function updateImg() {
+  for (let i = 0; i < imgs.length; i++) {
+    imgs[i].classList.remove('current');
+    imgs[i].classList.remove('next');
+    imgs[i].classList.remove('prev');
+  }
+
+  imgs[currImg].classList.add('current');
+
+  if (currImg === 0) {
+    imgs[imgs.length - 1].classList.add('prev');
+    imgs[currImg + 1].classList.add('next');
+  } else if (currImg === imgs.length - 1) {
+    imgs[currImg - 1].classList.add('prev');
+    imgs[0].classList.add('next');
+  } else {
+    imgs[currImg - 1].classList.add('prev');
+    imgs[currImg + 1].classList.add('next');
+  }
+}
+
+setInterval(() => {
+  currImg = (currImg + 1) % imgs.length;
+  updateImg();
+}, 3000);
+
+updateImg();
+
+
+
+
+
 // window.onload = function () {
 //   let slides =
 //       document.getElementsByClassName('carousel-item');
