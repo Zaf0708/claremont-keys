7ac47501183ea2c6e39db229a48a736b4d3f45b5
Carrimbaccus Zafroollah <zafroollah@gmail.com>
2024-03-30 17:01:25 +1100

    Changed image and add slider function.

--- script.js
+++ script.js
@@ -1,34 +1,73 @@
-const slider = document.querySelector('.slider');
-const imgs = slider.children;
-let currImg = 0;
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
 
-function updateImg() {
-  for (let i = 0; i < imgs.length; i++) {
-    imgs[i].classList.remove('current');
-    imgs[i].classList.remove('next');
-    imgs[i].classList.remove('prev');
-  }
-
-  imgs[currImg].classList.add('current');
-
-  if (currImg === 0) {
-    imgs[imgs.length - 1].classList.add('prev');
-    imgs[currImg + 1].classList.add('next');
-  } else if (currImg === imgs.length - 1) {
-    imgs[currImg - 1].classList.add('prev');
-    imgs[0].classList.add('next');
-  } else {
-    imgs[currImg - 1].classList.add('prev');
-    imgs[currImg + 1].classList.add('next');
-  }
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.opacity = 100;
+    } else {
+      slide.style.opacity = 0;
+    }
+  });
 }
 
-setInterval(() => {
-  currImg = (currImg + 1) % imgs.length;
-  updateImg();
-}, 3000);
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
 
-updateImg();
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+// Add event listeners for the navigation buttons
+document.querySelector(".prev").addEventListener("click", prevSlide);
+document.querySelector(".next").addEventListener("click", nextSlide);
+
+// Add a slide animation interval
+setInterval(nextSlide, 5000);
+
+
+
+
+
+
+
+
+// const slider = document.querySelector('.slider');
+// const imgs = slider.children;
+// let currImg = 0;
+
+// function updateImg() {
+//   for (let i = 0; i < imgs.length; i++) {
+//     imgs[i].classList.remove('current');
+//     imgs[i].classList.remove('next');
+//     imgs[i].classList.remove('prev');
+//   }
+
+//   imgs[currImg].classList.add('current');
+
+//   if (currImg === 0) {
+//     imgs[imgs.length - 1].classList.add('prev');
+//     imgs[currImg + 1].classList.add('next');
+//   } else if (currImg === imgs.length - 1) {
+//     imgs[currImg - 1].classList.add('prev');
+//     imgs[0].classList.add('next');
+//   } else {
+//     imgs[currImg - 1].classList.add('prev');
+//     imgs[currImg + 1].classList.add('next');
+//   }
+// }
+
+// setInterval(() => {
+//   currImg = (currImg + 1) % imgs.length;
+//   updateImg();
+// }, 3000);
+
+// updateImg();
 
 
 
